894f284fb4ae0e2aabf05e3f1f87e0191367c128
Racoth505 <rafafl2001@gmail.com>
2025-11-06 19:35:56 -0600

    Boton y comentarios

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 const icon = require('./assets/icon.png');
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <Image source={{uri: "https://wiki.leagueoflegends.com/en-us/images/Chibi_Gwen_Base_Tier_1.png?fc9c7"}} 
+      <Image source={{uri: "https://wiki.leagueoflegends.com/en-us/images/Chibi_Gwen_Base_Tier_1.png?fc9c7"}} /* Imagen cargada de internet */
         style={{
         width: 512, 
         height: 512,
         resizeMode: 'center'}}/>
 
-      <Text style={{color: 'white'}}>Inicio de la aplicacion</Text>
-      <StatusBar style="ligth" />
+      <Text style={{color: 'white'}}>{'Inicio de la aplicacion'}</Text> {/* Texto */}
+      <StatusBar style="light" /> {/* Color de la barra superior */}
+      <Button title="Pulsa aqui" onPress={() => alert('Hola')} /> {/* Prueba de boton */}
     </View>
   );
 }
