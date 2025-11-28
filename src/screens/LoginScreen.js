9b1e1a542173dca3500c5a7bc5e3339282317c77
Pollo-30 <vmsan30@gmail.com>
2025-11-28 08:12:49 -0600

    Saquen Brawl

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -13,7 +13,7 @@ import { useDualPress } from "../hooks/useDualPress";
 import { ScreenTitle } from "../components/ScreenTitle";
 import { styles } from "../styles/styles";
 
-const heartbeatLogo = require("../../assets/heartbeat.png");
+const heartbeatLogo = require("../../assets/heartbeat2.png");
 
 // URL de Lambda
 const API_URL =
